36501dc5f8a0eb1e3e77c2e67d34bc6dea695ab7
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-02-05 17:45:14 +0530

    database connected and routes for product updated'

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,54 +1,121 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var router = express.Router();
+var Product = require("../models/products");
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Handling GET request for /products"
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log("GET REQUEST\n", docs);
+        res.status(200).json(docs);
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
+    });
 });
 
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: "Handling POST request for /products"
-    })
+    var product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+
+    });
+
+    product.save()
+        .then(result => {
+            console.log("New Object Created\n", result);
+            res.status(201).json({
+                message: "Handling POST request for /products",
+                createdProduct: product
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error : err
+            });
+        });
+
+    
 });
 
 
 
 router.get("/:id", (req, res, next) => {
     var id = req.params.id;
-    if (id === "special") {
-        res.status(200).json({
-            message: "You discovered a special ID",
-            id: id
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("Fetched from Database\n", doc);
+            if(doc){
+                res.status(200).json(doc);
+            }else{
+                res.status(404).json({
+                    message : "No record found for the given id"
+                })
+            }
+            
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error : err
+            });
         });
-    } else {
-        res.status(200).json({
-            message: "You passed an ID",
-            id: id
-        });
-    }
 });
 
 
 router.delete("/:id", (req, res, next) => {
     var id = req.params.id;
-    res.status(200).json({
-        message: "You have deleted a product",
-        id: id
-    });
+    Product.findByIdAndDelete(id)
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        if(doc){
+            res.status(200).json(doc);
+        }else{
+            res.status(404).json({
+                error : {
+                    message : "No such record found"
+                }
+            })
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
+    })
 
 });
 
 
 router.patch("/:id", (req, res, next) => {
     var id = req.params.id;
-    res.status(200).json({
-        message: "Updated at ID",
-        id: id
-    });
-
+    var updateOps = {};
+    for(var ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({_id : id}, {$set : updateOps})
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        res.status(200).json(doc)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
+    })
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 var express = require("express");
-var app = express();
 var morgan = require("morgan");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var productRoutes = require("./api/routes/products");
 var orderRoutes = require("./api/routes/orders");
 
+
+var app = express();
+mongoose.connect("mongodb+srv://sanjaynithin:" + process.env.MONGO_PASSWORD + "@cluster0.kgz6ota.mongodb.net/?retryWrites=true&w=majority");
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Accept, Authorization, Content-Type");
+    if(req.method === 'OPTIONS'){
+        res.header(
+            "Access-Control-Allow-Methods", 
+            "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
 app.use("/products",productRoutes);
