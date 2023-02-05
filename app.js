6d318685dc622e5d6a00a9010b8cbceb68112f49
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-02-05 13:57:21 +0530

    nodemon module added and errors are handled

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var app = express();
+var morgan = require("morgan");
 var productRoutes = require("./api/routes/products");
 var orderRoutes = require("./api/routes/orders");
 
+app.use(morgan('dev'));
+
+
 app.use("/products",productRoutes);
 app.use("/orders", orderRoutes);
+
+app.use((req,res,next)=>{
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+app.use((error, req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message : error.message
+        }
+    });
+});
+
 module.exports = app;
 
